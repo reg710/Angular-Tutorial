440eef7ee4399a31cc5edac2984edd1dcf6b3705
rcon <reg710@gmail.com>
2022-01-02 15:08:29 -0500

    Through part 2 - covering ngFor, ngIf, and class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -10,12 +11,15 @@ export class HeroesComponent implements OnInit {
   
   constructor() { }
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  heroes = HEROES;
 
   ngOnInit(): void {
+  }
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    console.log("hi friend, " + hero.name);
   }
 
 }
